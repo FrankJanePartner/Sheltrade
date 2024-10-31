c4623b5167059938680ac36e61b97bf9e94cf9ee
Frank Jane Partner <96851698+FrankJanePartner@users.noreply.github.com>
2024-10-31 10:04:00 +0100

    Delete sheltrade/giftcard directory

--- sheltrade/giftcard/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import sellgiftcard, buygiftcard
-
-app_name = 'giftcard'
-
-urlpatterns = [
-    path('', buygiftcard, name='buygiftcard'),
-    path('sell/', sellgiftcard, name='sellgiftcard'),
-]
