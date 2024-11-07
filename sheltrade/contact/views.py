3ceed9fc90925fead186c56abd7d888c771f541e
FrankJanePartner <partnermarvel55@gmail.com>
2024-11-07 10:50:39 +0100

    Base template to all pages

--- sheltrade/contact/views.py
+++ sheltrade/contact/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def contactus(request):
-    return render(request, 'core/contactus.html')
+    if request.user.is_authenticated:
+        base_template = "../base.html"  # For authenticated users
+    else:
+        base_template = "base.html"  # For unauthenticated users
+
+    context = {
+        "user": request.user,  # No need to fetch from the database
+        "base_template": base_template,
+    }
+    return render(request, 'core/contactus.html', context)
